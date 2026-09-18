b5d4ccfcf4f6fb3c1a78bd75981ee80da4f6993e
rakeshbulli11 <rakeshbulli11@gmail.com>
2026-09-18 20:26:52 +0530

    security: remove hard-coded database credentials

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:REMOVED_SECRET@db:5432/aiopshub"
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
